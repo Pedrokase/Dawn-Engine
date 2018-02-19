1689ad046eafc7267c9e37899a8095d9d8699355
Pedrokase <pedrokase@hotmail.com>
2018-02-19 16:20:10 +0000

    Started Dawn Engine v.1.0

---
+++ src/io/github/pedrokase/DawnEngine.java
@@ -0,0 +1,17 @@
+package io.github.pedrokase;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DawnEngine extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
+
